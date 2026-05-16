85566fee299527578e5d8e0bdb039ab15e1c8875
Mridhul2k03 <mridhulkrishna11@gmail.com>
2026-05-16 21:47:49 +0530

    first commit

---
+++ BaseApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class Home(APIView):
+    def get(self, request):
+        return Response({"status":status.HTTP_200_OK,"Message": "Welcome to the API"})
